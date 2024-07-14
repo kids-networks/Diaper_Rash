36a6cb6e77fa44d79a9f9abf1d6f62da673a8209
Marcus Yeung <marcusy371@gmail.com>
2024-07-14 11:26:20 +0800

    Create calculator.py

---
+++ calculator/calculator.py
@@ -0,0 +1,11 @@
+num1 = float(input("Input first number."))
+num2 = float(input("Input second number."))
+method = input ("Input method.")
+if method == "+" :
+    print (num1 + num2)
+if method == "-" :
+    print (num1 - num2)
+if method == "*" :
+    print (num1 * num2)
+if method == "/" :
+    print (num1 / num2)
